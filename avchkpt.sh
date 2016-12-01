5484348954e3b7262cee6630c3b483de47a6814d
Eddie Pavkovic <eddie.pavkovic@emc.com>
2016-12-01 14:49:45 -0500

    Avamar checkpoint script 1.1, better, faster, stronger - Flushes mcserver data before checkpoint - Allows for full or rolling checkpoints - Better mgmt of checking progress of checkpoint

--- avchkpt.sh
+++ avchkpt.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-version="1.0"
+version="1.1"
 
 echomsg ()
 {
@@ -10,6 +10,11 @@ echomsg ()
 av_checkpoint_create(){
     mccli_opts=" --override_maintenance_scheduler\=true"
     tsleep=30
+    echomsg "Flushing MC Server data"
+
+    ## have to run mcserver.sh as admin 
+    su admin -c "mcserver.sh --flush" 
+
     echomsg "Creating a checkpoint"
     curdate=`date -u +"%Y%m%d"`
     cmd="mccli checkpoint create $mccli_opts"
@@ -29,23 +34,24 @@ av_checkpoint_create(){
     echomsg "Running cmd: $cmd"
     eval "$cmd"
     ##  made it here means we have a checkpoint to validate
-    cmd="mccli checkpoint validate --cptag=$cpname $mccli_opts"
+    cmd="mccli checkpoint validate --cptag=$cpname --checktype=$type $mccli_opts"
     echomsg "Validating the checkpoint $cpname"
     echomsg "Running cmd: $cmd"
     eval "$cmd"
+
     ## waiting for the checkpoint to get to In Progress state
-    echomsg "Waiting for the validation to start..."
-    cpstate=`mccli checkpoint show | grep "$cpname.*In Progress"`
-    while [ "$cpstate" = "" ]
+    echomsg "Waiting for the validation to be In Progress..."
+    cpstatecmd="mccli checkpoint show | grep \"$cpname.*In Progress\""
+    eval $cpstatecmd
+    while [ $? -eq 1  ]
     do
-	cpstate=`mccli checkpoint show | grep "$cpname.*In Progress"`
+	eval $cpstatecmd
     done
 
-    cpstate=`mccli checkpoint show | grep "$cpname.*In Progress"`
-    while [ "$cpstate" != "" ]
+    echo
+    while [ $? -ne 1 ]
     do
-	echomsg "Found $cpstate"
-	cpstate=`mccli checkpoint show | grep "$cpname.*In Progress"`
+	eval $cpstatecmd
     done
     cmd="mccli checkpoint show | grep $cpname"
     echomsg "Running cmd : $cmd"
@@ -54,6 +60,7 @@ av_checkpoint_create(){
 
 ##main()
 
+exitstatus=0
 echomsg "$0 version: $version"
 case $1 in 
     "--list")
@@ -63,12 +70,12 @@ case $1 in
 	num=1
 	if [ ! -z $2 ]
 	then 
-	    echo here
 	    num=$2
 	fi
+	type=${3:-"full"}
 	while (($num > 0 ))
 	do
-	    av_checkpoint_create
+	    av_checkpoint_create $type
             num=$(($num - 1))
         done
 	cmd="mccli checkpoint show"
@@ -76,12 +83,12 @@ case $1 in
 	eval "$cmd"
 	;;
     *)
-	echo "usage --list | --create [1 | 2]"
-	echo "   --list - list current checkpoints"
-        echo "   --create - create 1 or 2 checkpoints (default is 1 checkpoint)"
-        exit 0
+	echo "usage --list | --create [ 1 | 2 ] [ full | rolling ]"
+	echo "   --list   - list current checkpoints"
+        echo "   --create - create 1 or 2 checkpoints (default is 1), validate full or rolling"
         ;;
 esac
 
 echomsg "All done"
 
+exit $exitstatus
